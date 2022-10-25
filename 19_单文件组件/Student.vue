d33f9c4c19f40e424eb053f5b7c5160a7ee07fdd
Kevin <745337567@qq.com>
2022-10-25 09:43:54 +0800

    first commit

---
+++ 19_单文件组件/Student.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="demo2">
+    <h2>学生名字：{{ name }}</h2>
+    <h2>年龄：{{ address }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Student",
+  data() {
+    return {
+      name: "牛马",
+      age: 18,
+    };
+  },
+};
+</script>
---
+++ 19_单文件组件/main.js
@@ -0,0 +1,7 @@
+import App from './App.vue';
+var vm = new Vue({
+    el: '#root',
+    components: {
+        App
+    }
+});
